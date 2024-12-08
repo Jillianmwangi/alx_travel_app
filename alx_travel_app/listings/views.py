903214fa04dc385c4f0e6a73893426559ff85a96
Jill Mwangi <122970681+Jillianmwangi@users.noreply.github.com>
2024-12-08 23:39:15 +0300

    Add new travel app files

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, World!"})
+
